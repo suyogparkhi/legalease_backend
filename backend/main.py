4fb0ad293c32e2896bd09fee22eca833b8b4be09
suyogparkhi <suyogp123456789@gmail.com>
2024-11-30 04:19:35 +0530

    changes

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ settings = get_settings()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
